272186d2b21cbf11e214e399c39cc2215654cf11
Rob <robert.erez@gmail.com>
2018-02-11 18:52:03 +1000

    N

---
+++ index.js
@@ -0,0 +1,12 @@
+exports.handler = (event, context, callback) => {
+    
+// TODO implement
+    console.log("HH");
+	
+	callback(null, {
+		"customParam": process.env.CustomParameter,
+		"environment": process.env.Environment,
+		"Release": process.env.Release
+	});
+
+};
