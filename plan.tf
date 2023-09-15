6c92c7dd55d3deacb78839d966051a69869b5a59
Rob E <robert.erez@gmail.com>
2023-09-15 10:11:17 +1000

    Update plan.tf

--- plan.tf
+++ plan.tf
@@ -2,7 +2,8 @@ provider “aws” {
     region = “us-west-1”
 }
  
-resource “aws_instance” “myec2” {
+resource “aws_instance” “myec2-prod” {
     ami = “ami-12345qwert”
     instance_type = “t2.micro”
 }
+
